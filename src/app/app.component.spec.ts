e50484087c233287d78ae736522059b2a15930d4
Ikhy <markkcodi@hotmail.es>
2019-03-26 17:59:35 +0100

    new branch. Solucionado el problema del rol.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'pt32Parelles'`, () => {
+  it(`should have as title 'PracticaAngular'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('pt32Parelles');
+    expect(app.title).toEqual('PracticaAngular');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to pt32Parelles!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to PracticaAngular!');
   });
 });
